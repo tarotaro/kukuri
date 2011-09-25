31981d938521e7011d7ecde5a56573b60f54114e
Taro Nakamura <ntaro@livedoor.jp>
2011-09-25 16:41:33 +0900

    プロジェクト生成

---
+++ src/com/freeworks/kukuri/KukuriActivity.java
@@ -0,0 +1,13 @@
+package com.freeworks.kukuri;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KukuriActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
